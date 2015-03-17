a132a66c1da52f580a5eca62dec1029c1b47c88e
pr3ona <preona@gmail.com>
2015-03-17 22:12:13 +0200

    changed the array to 1d for easier comparision

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include "tictactoe.h"
 using namespace std;
+#include <string>
 int main()
 {
 
-int array[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+string array[10] = { "0","1","2","3","4","5","6","7","8","9"};
 
-cout << array[0][0] << array[0][1] << array[0][2] << endl;
-cout << array[1][0] << array[1][1] << array[1][2] << endl;
-cout << array[2][0] << array[2][1] << array[2][2] << endl;
+cout << array[1] << array[2] << array[3] << endl;
+cout << array[4] << array[5] << array[6] << endl;
+cout << array[7]<< array[8] << array[9] << endl;
+
+int num;
+
+
+cout << "player one please enter the number where you would like to place an x" << endl;
+cin >> array[num];
 
 
 system("pause");
