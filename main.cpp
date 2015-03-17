adde31e41a62e3b2c658646904c2423011fde388
pr3ona <preona@gmail.com>
2015-03-17 22:24:07 +0200

    user required to input number corresponding to position

--- main.cpp
+++ main.cpp
@@ -12,12 +12,17 @@ cout << array[4] << array[5] << array[6] << endl;
 cout << array[7]<< array[8] << array[9] << endl;
 
 int num;
+int flag = 0;
+string string1 = "x";
+string strng2 = "o";
 
 
+while (flag =0)
 cout << "player one please enter the number where you would like to place an x" << endl;
 cin >> array[num];
 
-
+if (array[num].compare(string1)!=0)
+array[num] = string1;
 system("pause");
 
 }
