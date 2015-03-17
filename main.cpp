0cc7a8b8ac96cc4afdd15788b510cad964c731ee
pr3ona <preona@gmail.com>
2015-03-17 13:39:26 +0200

    created 2d array to store x and o's

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "tictactoe.h"
+using namespace std;
+int main()
+{
+
+int array[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+cout << array[0][0] << array[0][1] << array[0][2] << endl;
+cout << array[1][0] << array[1][1] << array[1][2] << endl;
+cout << array[2][0] << array[2][1] << array[2][2] << endl;
+
+
+system("pause");
+
+}
+
