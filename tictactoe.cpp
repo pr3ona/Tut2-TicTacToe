4f47c9db618968806b86df5fa7caaef0fc805c9c
pr3ona <preona@gmail.com>
2015-03-22 12:46:29 +0200

    array size change

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -11,9 +11,8 @@ string array[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 void restart()
 {
 
-	string array[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	string array[9] = { "0", "1", "2", "3", "4", "5", "6", "7", "8"};
 
-	
 
 };
 
@@ -22,4 +21,11 @@ void print()
 	cout << array[1] << array[2] << array[3] << endl;
 	cout << array[4] << array[5] << array[6] << endl;
 	cout << array[7] << array[8] << array[9] << endl;
+};
+
+int isWon()
+{
+
+
+
 };
