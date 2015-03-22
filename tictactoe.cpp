d87d98ef26351a0a6474447085918746b776e855
pr3ona <preona@gmail.com>
2015-03-22 12:42:29 +0200

    add print and restart function

---
+++ tictactoe.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include "tictactoe.h"
+using namespace std;
+#include <string>
+
+
+string array[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+
+void restart()
+{
+
+	string array[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+	
+
+};
+
+void print()
+{
+	cout << array[1] << array[2] << array[3] << endl;
+	cout << array[4] << array[5] << array[6] << endl;
+	cout << array[7] << array[8] << array[9] << endl;
+};
